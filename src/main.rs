57b4eca336e8cf95fd21f935bc45851ae5d77295
tukanoidd <tukanoid@outlook.com>
2022-01-26 15:38:46 +0100

    just easier to add more parameters later

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,68 @@
-pub mod project;
 pub mod file_info;
 pub mod node;
+pub mod project;
 
-use std::{
-    env,
-    rc::Rc,
-};
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
+use std::fmt::{Display, Formatter};
+use std::{env, fs::File, io::Write, path::Path, rc::Rc};
+
+use enum_map::{enum_map, Enum, EnumMap};
 use itertools::Itertools;
 
-use substring::Substring;
+use crate::{node::Node, project::Project};
 
-use crate::{
-    node::Node,
-    project::Project
-};
+#[derive(Enum, Clone, Copy)]
+enum ParamType {
+    Project,
+    EntryPoint,
+    Output,
+}
+
+impl From<&str> for ParamType {
+    fn from(val: &str) -> Self {
+        match val {
+            "p" => ParamType::Project,
+            "e" => ParamType::EntryPoint,
+            "o" => ParamType::Output,
+            _ => panic!("Invalid Argument: {}", val),
+        }
+    }
+}
+
+impl Display for ParamType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                ParamType::Project => "Project",
+                ParamType::EntryPoint => "Entry Point",
+                ParamType::Output => "Output File",
+            }
+        )
+    }
+}
 
 const PARAM_SYMBOL: &'static str = "~";
+
+fn param_exists_err(param_types: &[ParamType], map: &EnumMap<ParamType, &str>) -> Option<String> {
+    let mut res = String::new();
+
+    for &param_type in param_types {
+        if map[param_type].is_empty() {
+            res.push_str(&format!(
+                "{}{}",
+                if res.is_empty() { "" } else { " & " },
+                param_type
+            ));
+        }
+    }
+
+    if res.is_empty() {
+        None
+    } else {
+        Some(res)
+    }
+}
 
 /// ~p={project_path} (has to be absolute path)
 /// ~e={entry_point} (has to be absolute path)
@@ -26,43 +70,40 @@ const PARAM_SYMBOL: &'static str = "~";
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let mut project_path = "";
-    let mut entry_point = "";
-    let mut output_file = "";
+    let mut params = enum_map! {
+        ParamType::Project => "",
+        ParamType::EntryPoint => "",
+        ParamType::Output => "",
+    };
 
     for arg in args.iter() {
         if arg.contains(PARAM_SYMBOL) {
-            let arg_len = arg.len();
-            let arg_val = arg.substring(3, arg_len);
-
-            if arg.starts_with(format!("{}p", PARAM_SYMBOL).as_str()) {
-                project_path = arg_val.clone();
-            } else if arg.starts_with(format!("{}e", PARAM_SYMBOL).as_str()) {
-                entry_point = arg_val.clone();
-            } else if arg.starts_with(format!("{}o", PARAM_SYMBOL).as_str()) {
-                output_file = arg_val.clone();
-            }
+            params[ParamType::from(&arg[1..2])] = (&arg[3..]).clone();
         }
     }
 
-    if project_path.is_empty() || entry_point.is_empty() || output_file.is_empty() {
-        panic!("Project/Entry Point/Output File Path was not set!")
+    if let Some(err) = param_exists_err(
+        &[ParamType::Project, ParamType::EntryPoint, ParamType::Output],
+        &params,
+    ) {
+        let multiple = err.contains("&");
+        panic!(
+            "{} File Path{} {} not set!",
+            err,
+            if multiple { "s" } else { "" },
+            if multiple { "were" } else { "was" }
+        )
     }
 
-    let mut project = Project::create(project_path.to_owned());
-    let entry_point_file_info = Rc::new(
-        project.create_file_info(entry_point.to_owned())
-    );
+    let mut project = Project::create(params[ParamType::Project].to_owned());
+    let entry_point_file_info =
+        Rc::new(project.create_file_info(params[ParamType::EntryPoint].to_owned()));
 
-    let root_node = Node::create(
-        &entry_point_file_info,
-        None,
-    );
+    let root_node = Node::create(&entry_point_file_info, None);
 
-    let recursive_paths =
-        Node::traverse(&root_node, &mut project);
+    let recursive_paths = Node::traverse(&root_node, &mut project);
 
-    let mut file = File::create(Path::new(output_file))
+    let mut file = File::create(Path::new(params[ParamType::Output]))
         .expect("Couldn't create the output file!");
 
     for (file_name, paths) in recursive_paths.iter() {
@@ -72,10 +113,9 @@ fn main() {
         file.write((format!("{}:\n", file_name)).as_bytes())
             .expect("Couldn't write to the output file");
 
-        let output_paths: Vec<&Vec<String>> = paths.iter()
-            .sorted_by(|path1, path2| {
-                Ord::cmp(&path1.len(), &path2.len())
-            })
+        let output_paths: Vec<&Vec<String>> = paths
+            .iter()
+            .sorted_by(|path1, path2| Ord::cmp(&path1.len(), &path2.len()))
             .collect();
 
         for path in output_paths {
